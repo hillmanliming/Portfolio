372a8039a339c8dc3c8afabe491ee72eb2eee4b3
Li-Ming Hillman <liming@hillmanconsult.com>
2025-10-17 20:27:31 +0200

    Add Prettier configuration and ignore file for formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,43 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-6">
-      <Image
-        src="/Logo.svg"
-        alt="Li-Ming Hillman Logo"
-        width={90}
-        height={19}
-        priority
-      />
-      <div className="text-xl">Coming soon...</div>
-    </div>
+    <>
+      <header className="fixed top-0 left-0 w-full p-8">
+        <nav>
+          <ul className="flex items-center justify-between">
+            <li>
+              <a href="">
+                <Image
+                  src="/Logo.svg"
+                  alt="Logo"
+                  width={21}
+                  height={30}
+                  priority
+                />
+              </a>
+            </li>
+            <div className="flex gap-9">
+              <li className="right flex">
+                <a href="">About</a>
+              </li>
+              <li>
+                <a href="">Contact</a>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </header>
+      <main className="height-screen m-8 flex flex-col items-center">
+        <h1 className="text-[200px] max-2xl:text-[180px] max-xl:text-[140px]">
+          SIMPLICITY
+        </h1>
+        <p className="max-w-sm text-center text-base italic">
+          "...the three pillars of my development philosophy. Qualities that I
+          believe perfectly capture the expectations of modern web development.”
+          -
+        </p>
+        <p>Li-Ming Hillman</p>
+      </main>
+    </>
   );
 }
